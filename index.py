9eccbcbd52c7b60fe3a410a0c04fe1c431e11aa0
Divya Rani <ranidivya063@gmail.com>
2017-02-07 20:18:07 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,46 @@
+
+# The key idea of this program is to equate the strings
+# "rock", "paper", "scissors", "lizard", "Spock" to numbers
+# as follows:
+#
+# 0 - rock
+# 1 - Spock
+# 2 - paper
+# 3 - lizard
+# 4 - scissors
+
+# helper functions
+
+def name_to_number(name):
+    # delete the following pass statement and fill in your code below
+    if (name=="rock"):
+        return 0
+    elif (name=="Spock"):
+        return 1
+    elif(name=="paper"):
+        return 2
+    elif(name=="lizard"):
+        return 3
+    elif(name=="scissors"):
+        return 4
+    else:
+        print 'error'
+
+    # convert name to number using if/elif/else
+    # don't forget to return the result!
+
+
+def number_to_name(number):
+    # delete the following pass statement and fill in your code below
+    if(number==0):
+        return "rock"
+    elif(number==1):
+        return "Spock"
+    elif(number==2):
+        return "paper"
+    elif(number==3):
+        return "lizard"
+    elif(number==4):
+        return "scissors"
+    else:
+        print 'error'
